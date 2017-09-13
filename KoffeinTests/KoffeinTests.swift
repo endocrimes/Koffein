ae3b38914661b40a9207a4723acd75f347e73181
Danielle Tomlinson <dan@tomlinson.io>
2017-09-13 15:59:53 +0200

    Initial Commit

---
+++ KoffeinTests/KoffeinTests.swift
@@ -0,0 +1,36 @@
+//
+//  KoffeinTests.swift
+//  KoffeinTests
+//
+//  Created by Danielle Tomlinson on 13/09/2017.
+//  Copyright © 2017 Terrible Systems. All rights reserved.
+//
+
+import XCTest
+@testable import Koffein
+
+class KoffeinTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
